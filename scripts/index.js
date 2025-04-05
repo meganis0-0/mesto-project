adcc80a1b4afb4f1b6438549b2d81236ffa2966c
MEGANIS <->
2025-04-05 12:16:15 +0300

    Add comments + function documentation

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,42 @@
+//Initialize all popups
 const profilePopup = document.querySelector('.popup_type_edit');
 const cardPopup = document.querySelector('.popup_type_new-card');
 const imagePopup = document.querySelector('.popup_type_image');
 
+//Initialize template for cards
 const cardTemplate = document.querySelector('#card-template').content;
 const cardContainer = document.querySelector('.places__list');
 
+//Initialize profile
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
+//Initialize profile edit popup buttons
 const profileEditButton = document.querySelector('.profile__edit-button');
-const profileEditCloseButton = document.querySelector('.popup_type_edit').querySelector('.popup__close');
+const profileEditCloseButton = document.querySelector('.popup_type_edit .popup__close');
+//const profileEditCloseButton = document.querySelector('.popup_type_edit').querySelector('.popup__close');
 
+//Initialize add card popup buttons
 const profileAddButton = document.querySelector('.profile__add-button');
-const profileAddButtonClose = document.querySelector('.popup_type_new-card').querySelector('.popup__close');
+const profileAddButtonClose = document.querySelector('.popup_type_new-card .popup__close');
 
+
+//Events for edit profile
 profileEditButton.addEventListener('click', () => {
     openModal(profilePopup);
     profilePopup.querySelector('.popup__input_type_name').value = profileName.textContent;
     profilePopup.querySelector('.popup__input_type_description').value = profileDescription.textContent;
 
-    const profileEditForm = document.querySelector('.popup_type_edit').querySelector('.popup__form');
+    const profileEditForm = document.querySelector('.popup_type_edit .popup__form');
     //const profileEditSubmitButton = document.querySelector('.popup_type_edit').querySelector('.popup__button');
 
     const nameInput = profileEditForm.querySelector('.popup__input_type_name');
     const jobInput = profileEditForm.querySelector('.popup__input_type_description');
 
+    /**
+     * Function for submiting an edited profile
+     * @param {Event} evt - Event
+     */
     function handleProfileFormSubmit(evt) {
         evt.preventDefault();
 
@@ -43,6 +55,8 @@ profileEditCloseButton.addEventListener('click', () => {
     closeModal(profilePopup);
 })
 
+
+//Events for adding new card
 profileAddButton.addEventListener('click', () => {
     openModal(cardPopup);
 })
@@ -51,14 +65,27 @@ profileAddButtonClose.addEventListener('click', () => {
     closeModal(cardPopup);
 })
 
+/**
+ * Function for open popup
+ * @param {popup} popup - popup
+ */
 function openModal(popup) {
     popup.classList.add('popup_is-opened');
 }
 
+/**
+ * Function for close popup
+ * @param {popup} popup - popup
+ */
 function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
 }
 
+/**
+ * Function for creating new card using template
+ * @param {Element} card - Card which will be created
+ * @returns 
+ */
 function createCard(card) {
         const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
         cardElement.querySelector('.card__title').textContent = card.name;
@@ -66,6 +93,8 @@ function createCard(card) {
         return cardElement;
 }
 
+
+//When page open --> create card for each element of array from cards.js
 initialCards.forEach((card) => {
     const cardElement = createCard(card);
     cardContainer.append(cardElement);
